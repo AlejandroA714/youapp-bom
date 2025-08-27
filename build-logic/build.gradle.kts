5a8f9ffd4dcb2fd6f688652a3b2af373ab7ab6ac
Alejandro A <alejandroalejo714@gmail.com>
2025-08-26 18:39:21 -0600

    🚧 WIP

--- build-logic/build.gradle.kts
+++ build-logic/build.gradle.kts
@@ -1,8 +1,34 @@
 plugins {
-    `kotlin-dsl`
+    kotlin("jvm") version "2.2.0"
+    `java-gradle-plugin`
+    `maven-publish`
 }
+
+group = "com.sv.youapp.infrastructure"
+version = "1.0.0-SNAPSHOT"
 
 repositories {
     gradlePluginPortal()
     mavenCentral()
 }
+
+dependencies {
+    implementation("com.diffplug.spotless:spotless-plugin-gradle:6.25.0")
+}
+
+kotlin {
+    jvmToolchain(21)
+}
+
+gradlePlugin {
+    plugins {
+        create("formatterPlugin") {
+            id = "com.sv.youapp.formatter"
+            implementationClass = "com.sv.youapp.formatter.FormatterPlugin"
+            displayName = "YouApp Formatter Plugin"
+            description = "Aplica y configura Spotless (ktlint, googleJavaFormat, etc.)"
+        }
+    }
+}
+
+
